9fbe5bb5258ff762a4dc3d0aff8a9c1c8b023df6
Florin Popescu <florin@ltsdevelopment.com>
2017-09-13 08:41:23 +0300

    Updated testing env configuration

--- tests/Unloq/EnvironmentTest.php
+++ tests/Unloq/EnvironmentTest.php
@@ -10,5 +10,5 @@ class EnvironmentTest
 {
     const ENDPOINT_PROD = 'https://api.unloq.io/v1/';
     const ENDPOINT_CMS = 'https://cms.unloq.io/v1/';
-    const APP_KEY = 'live_5fQfdsyl3C8ZW2Z59UFuuZgTe8bdcce4ea2a40a59b42e9e9';
+    const APP_KEY = 'live_6UWEvITcMGuOAIqaFoVw2NN76680b2928bab47c4ba1200c6';
 }
